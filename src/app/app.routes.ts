789744843a5cb8d7349522f58e03ea8857e968e7
sodar-business <vargagabi2209@gmail.com>
2024-08-13 20:16:21 +0200

    static.yaml test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home'},
+    { path: 'home', component: AppComponent },
+    { path: '**', pathMatch: 'full', redirectTo: ''}
+];
