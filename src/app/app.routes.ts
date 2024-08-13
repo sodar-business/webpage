f6c019d712747fbb37c7dd0a8d970df1f3f1bbfd
sodar-business <vargagabi2209@gmail.com>
2024-08-13 20:36:47 +0200

    static.yaml test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-    { path: '', pathMatch: 'full', redirectTo: 'home'},
-    { path: 'home', component: AppComponent },
-    { path: '**', pathMatch: 'full', redirectTo: ''}
+    // { path: '', pathMatch: 'full', redirectTo: 'home'},
+    // { path: 'home', component: AppComponent },
+    // { path: '**', pathMatch: 'full', redirectTo: ''}
 ];
